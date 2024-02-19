d1be872e737414f4f4b7ad4c4c4f837ce47788b8
DilshodMura <75676999+DilshodMura@users.noreply.github.com>
2024-02-19 06:34:30 -0500

    init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello World!
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
